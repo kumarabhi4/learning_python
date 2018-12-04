413113fe842bd397b1cff9a5908abb6e1bd4d50d
kumarabhi4 <abhinit.in@gmail.com>
2018-12-04 10:23:18 +0530

    world city traversed

---
+++ world_city.py
@@ -0,0 +1,16 @@
+city = {"IND": {"MH":["Pune", "Mumbai", "Nagpur"],
+                "GUJ":["Ahmadabad", "Gandhinagar", "Surat"],
+                "UP":["Lucknow", "Kanpur", "Noida"],
+                "KA":["Bangalore", "Maysuru"]},
+         "US":{"CAL":["Los Angles", "San Fransisco", "Oakland"],
+               "FL":["Miami", "Orlando", "Tampa"],
+               "TX":["Houston", "Dallas", "Austin"]}
+       }
+
+
+for country in city:
+    print(country)
+    for state in city[country]:
+        print("\t"+state)
+        for wcity in city[country][state]:
+            print("\t\t"+wcity)
